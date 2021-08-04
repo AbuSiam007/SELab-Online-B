18cdfa20657d693c73f509508bf8d2bf82f0a3bf
Abu Siam <asiam173007@bscse.uiu.ac.bd>
2021-08-04 15:15:59 +0600

    change funcion inside sum_arr.c file

--- sum_arr.c
+++ sum_arr.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int sum(int arr[], int n) {
-    for (int i=0;i<n;i++){
-        arr[i] += 5;
+    int s = 0;
+    int i;
+    for (i=0;i<n;i++){
+        s += arr[i];
     }
-    Return arr[n-1];
+    return s;
 }
 int main() {
     int arr[100];
